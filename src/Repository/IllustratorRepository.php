17fdd06790d284db24b2537a0fea9b8a964cd818
furalys <fura.illus@gmail.com>
2022-11-17 19:54:33 +0100

    add entites

---
+++ src/Repository/IllustratorRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Illustrator;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Illustrator>
+ *
+ * @method Illustrator|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Illustrator|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Illustrator[]    findAll()
+ * @method Illustrator[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class IllustratorRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Illustrator::class);
+    }
+
+    public function save(Illustrator $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Illustrator $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Illustrator[] Returns an array of Illustrator objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('i')
+//            ->andWhere('i.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('i.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Illustrator
+//    {
+//        return $this->createQueryBuilder('i')
+//            ->andWhere('i.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
